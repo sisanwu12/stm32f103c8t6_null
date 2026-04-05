8bc2d4b2b1e282a82fe5621a020ba93d49739fc1
sisanwu12 <sisanwu12@outlook.com>
2026-04-05 17:47:57 +0800

    完善 FLASH 操作函数，新增半周期访问和延迟等级设置功能；更新寄存器地址定义以统一命名

--- Libraries/driver/dri_ll_flash.h
+++ Libraries/driver/dri_ll_flash.h
@@ -19,17 +19,17 @@
 /* ---------- FLASH 地址定义层 ---------- */
 
 /* FLASH 基地址 */
-#define FLASH_BASE_ADDR 0x40022000UL // FLASH寄存器基地址
+#define DRI_LL_FLASH_BASE_ADDR 0x40022000UL // FLASH寄存器基地址
 
 /* FLASH 寄存器偏移地址*/
-#define FLASH_ACR_OFFSET     0x00UL // 访问控制寄存器
-#define FLASH_KEYR_OFFSET    0x04UL // 密钥寄存器
-#define FLASH_OPTKEYR_OFFSET 0x08UL // 选项密钥寄存器
-#define FLASH_SR_OFFSET      0x0CUL // 状态寄存器
-#define FLASH_CR_OFFSET      0x10UL // 控制寄存器
-#define FLASH_AR_OFFSET      0x14UL // 地址寄存器
-#define FLASH_OBR_OFFSET     0x1CUL // 选项字节寄存器
-#define FLASH_WRPR_OFFSET    0x20UL // 写保护寄存器
+#define DRI_LL_FLASH_ACR_OFFSET     0x00UL // 访问控制寄存器
+#define DRI_LL_FLASH_KEYR_OFFSET    0x04UL // 密钥寄存器
+#define DRI_LL_FLASH_OPTKEYR_OFFSET 0x08UL // 选项密钥寄存器
+#define DRI_LL_FLASH_SR_OFFSET      0x0CUL // 状态寄存器
+#define DRI_LL_FLASH_CR_OFFSET      0x10UL // 控制寄存器
+#define DRI_LL_FLASH_AR_OFFSET      0x14UL // 地址寄存器
+#define DRI_LL_FLASH_OBR_OFFSET     0x1CUL // 选项字节寄存器
+#define DRI_LL_FLASH_WRPR_OFFSET    0x20UL // 写保护寄存器
 
 /* ---------- 寄存器位定义 ---------- */
 
@@ -57,14 +57,27 @@ typedef enum
     DRI_LL_FLASH_LATENCY_2 = 0x02UL, // 2 等级 (48MHz < SYSCLK <= 72MHz)
 } dri_ll_flash_latency;
 
+/* 字段位移系数 */
+typedef enum
+{
+    FLASH_ACR_LATENCY_POS = 0,
+} dri_ll_flash_field_pos;
+
+/* ========== 对外接口 ========== */
+
+/* ---------- FLASH操作函数 ---------- */
+
+/* FLASH预取指函数 */
 void     dri_ll_flash_prefetch_enable(void);
 void     dri_ll_flash_prefetch_disable(void);
 isENABLE dri_ll_flash_prefetch_is_enabled(void);
 
+/* 半周期访问函数 */
 void     dri_ll_flash_halfcycle_enable(void);
 void     dri_ll_flash_halfcycle_disable(void);
 isENABLE dri_ll_flash_halfcycle_is_enabled(void);
 
+/* FLASH延迟等级设置函数 */
 void                 dri_ll_flash_latency_set(dri_ll_flash_latency latency);
 dri_ll_flash_latency dri_ll_flash_latency_get(void);
 
